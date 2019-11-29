df6720b77ae82a1ff3731be1c68a2c5b3cadb9a0
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-11-29 20:58:16 +0000

    status code fix

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,3 @@
 exports.getHome = (req, res, next) => {
-    res.status(404).render('main/index', {pageTitle: "Node.js Starter", path: '/'});
+    res.render('main/index', {pageTitle: "Node.js Starter", path: '/'});
 };
